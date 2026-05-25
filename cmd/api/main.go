b28ce36a9241c155390452fcf63fd08de6c653ff
RustambekJavascript <rustambekeshboriyev@gmail.com>
2026-05-25 07:18:13 +0500

    feat: configuration config file and connect to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/JavascriptDev347/learning-go-shop/internal/config"
+	"github.com/JavascriptDev347/learning-go-shop/internal/database"
+	"github.com/gin-gonic/gin"
+
+	"github.com/JavascriptDev347/learning-go-shop/internal/logger"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	// Initialize logger
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load configuration")
+	}
+
+	// connect to db
+	db, err := database.New(cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to database")
+	}
+
+	mainDb, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get database")
+	}
+	defer mainDb.Close()
+
+	gin.SetMode(cfg.Server.GinMode)
+
+	log.Info().Msg("Starting server")
 }
---
+++ internal/config/config.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+	JWT      JWTConfig
+	AWS      AWSConfig
+	Upload   UploadConfig
+}
+type ServerConfig struct {
+	Port    string
+	GinMode string
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+type JWTConfig struct {
+	Secret                string
+	ExpiresIn             time.Duration
+	RefreshTokenExpiresIn time.Duration
+}
+
+type AWSConfig struct {
+	AccessKeyID     string
+	SecretAccessKey string
+	Region          string
+	S3Bucket        string
+	S3Endpoint      string
+}
+
+type UploadConfig struct {
+	Path        string
+	MaxFileSize int64
+}
+
+func Load() (*Config, error) {
+	_ = godotenv.Load()
+
+	jwtExpiresIn, _ := time.ParseDuration(getEnv("JWT_EXPIRES_IN", "15m"))
+	refreshTokenExpiresIn, _ := time.ParseDuration(getEnv("JWT_REFRESH_TOKEN_EXPIRES_IN", "7d"))
+	maxUploadSize, _ := strconv.ParseInt(getEnv("MAX_UPLOAD_SIZE", "10485760"), 10, 64) // 10 MB
+	return &Config{
+		Server: ServerConfig{
+			Port:    getEnv("PORT", "8080"),
+			GinMode: getEnv("GIN_MODE", "debug"),
+		},
+		Database: DatabaseConfig{
+			Host:     getEnv("DATABASE_HOST", "localhost"),
+			Port:     getEnv("DATABASE_PORT", "5432"),
+			User:     getEnv("DATABASE_USER", "postgres"),
+			Password: getEnv("DATABASE_PASSWORD", "postgres"),
+			Name:     getEnv("DATABASE_NAME", "go_ecommerce_shop"),
+			SSLMode:  getEnv("DATABASE_SSLMODE", "disable"),
+		},
+		JWT: JWTConfig{
+			Secret:                getEnv("JWT_SECRET", "secret"),
+			ExpiresIn:             jwtExpiresIn,
+			RefreshTokenExpiresIn: refreshTokenExpiresIn,
+		},
+		AWS: AWSConfig{
+			AccessKeyID:     getEnv("AWS_ACCESS_KEY_ID", "test"),
+			SecretAccessKey: getEnv("AWS_SECRET_ACCESS_KEY", "test"),
+			Region:          getEnv("AWS_REGION", "us-east-1"),
+			S3Bucket:        getEnv("AWS_S3_BUCKET", "ecommerce-uploads"),
+			S3Endpoint:      getEnv("AWS_S3_ENDPOINT", "http://localhost:9000"),
+		},
+		Upload: UploadConfig{
+			Path:        getEnv("UPLOAD_PATH", "./uploads"),
+			MaxFileSize: maxUploadSize,
+		},
+	}, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
